acdeae7cef7523e71ad1a7be25c664a6529639e4
JosueDurand96 <josue.durand8@gmail.com>
2019-03-26 01:34:11 -0500

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -30,15 +30,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   Future<List<User>> _getUsers() async {
-   var data = await http.get("http://www.json-generator.com/api/json/get/cfwZmvEBbC?indent=2");
-  //  var data = await http.post("http://192.168.1.61:8084/api/administrador/find?=1");
+    var data = await http
+        .get("http://www.json-generator.com/api/json/get/cfwZmvEBbC?indent=2");
+    //  var data = await http.post("http://192.168.1.61:8084/api/administrador/find?=1");
 
     var jsonData = json.decode(data.body);
     List<User> users = [];
     for (var u in jsonData) {
-      User user = User(u["index"], u["about"], u["name"], u["email"], u["picture"]);
+      User user =
+          User(u["index"], u["about"], u["name"], u["email"], u["picture"]);
       users.add(user);
     }
 
@@ -63,71 +64,239 @@ class _MyHomePageState extends State<MyHomePage> {
               return ListView.builder(
                 itemCount: snapshot.data.length,
                 itemBuilder: (BuildContext context, int index) {
-                    return Column(
+                  Padding(
+                    padding: EdgeInsets.only(top: 10),
+                  );
+                  return Card(
+                    margin: EdgeInsets.only(
+                        left: 10, right: 10, bottom: 10, top: 10),
+                    child: Column(
                       children: <Widget>[
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
-
                             Container(
-                              margin: EdgeInsets.only(left: 30, right: 15),
+                              margin: EdgeInsets.only(
+                                left: 5,
+                                right: 15,
+                              ),
                               padding: EdgeInsets.only(top: 10),
                               height: 45,
-                              width: 240,
+                              width: 190,
                               decoration: BoxDecoration(
                                 color: Colors.white,
-                                border: Border(
-                                  bottom: BorderSide(
-                                      color: Colors.black54,
-                                      style: BorderStyle.solid),
-                                ),
                               ),
-                              child: TextFormField(
+                              child: TextField(
                                 //  controller: departamentoControll,
                                 decoration: InputDecoration.collapsed(
-                                  hintText: snapshot.data[index].name,
-
+                                  hintText: 'Nombre:',
                                 ),
-
                               ),
                             ),
                             Container(
                               margin: EdgeInsets.only(left: 0),
-                              padding: const EdgeInsets.only(top:10.0,bottom: 4.0),
+                              padding:
+                                  const EdgeInsets.only(top: 10.0, bottom: 4.0),
                               height: 45,
-                              width: 70,
+                              width: 180,
                               decoration: BoxDecoration(
                                 color: Colors.white,
-                                border: Border(
-                                  bottom: BorderSide(
-
-                                      color: Colors.black54,
-                                      style: BorderStyle.solid),
-                                ),
                               ),
-
-                              child: RaisedButton(
+                              child: TextFormField(
+                                decoration: InputDecoration.collapsed(
+                                  hintText: snapshot.data[index].name,
+                                ),
                                 //controller: provinciaControll,
-                                onPressed: (){},
-                                textColor: Colors.white,
-                                color: Colors.green,
-                                // padding: const EdgeInsets.only(top:10.0,bottom: 14.0),
-                                child: new Icon(
-                                  Icons.file_upload,
-                                  size: 26,
-                                ),
-
                               ),
-
                             ),
-                            Padding(
-                              padding: EdgeInsets.only(),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(
+                                left: 5,
+                                right: 15,
+                              ),
+                              padding: EdgeInsets.only(top: 10),
+                              height: 45,
+                              width: 190,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: TextFormField(
+                                //  controller: departamentoControll,
+                                decoration: InputDecoration.collapsed(
+                                  hintText: 'Tarifa Diurna:',
+                                ),
+                              ),
                             ),
-
+                            Container(
+                              margin: EdgeInsets.only(left: 0),
+                              padding:
+                                  const EdgeInsets.only(top: 10.0, bottom: 4.0),
+                              height: 45,
+                              width: 180,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: TextFormField(
+                                decoration: InputDecoration.collapsed(
+                                  hintText: snapshot.data[index].name,
+                                ),
+                                //controller: provinciaControll,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(
+                                left: 5,
+                                right: 15,
+                              ),
+                              padding: EdgeInsets.only(top: 10),
+                              height: 45,
+                              width: 190,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: TextFormField(
+                                //  controller: departamentoControll,
+                                decoration: InputDecoration.collapsed(
+                                  hintText: 'Tarifa Nocturna:',
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 0),
+                              padding:
+                                  const EdgeInsets.only(top: 10.0, bottom: 4.0),
+                              height: 45,
+                              width: 180,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: TextFormField(
+                                decoration: InputDecoration.collapsed(
+                                  hintText: snapshot.data[index].name,
+                                ),
+                                //controller: provinciaControll,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(
+                                left: 5,
+                                right: 15,
+                              ),
+                              padding: EdgeInsets.only(top: 10),
+                              height: 45,
+                              width: 190,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: TextFormField(
+                                //  controller: departamentoControll,
+                                decoration: InputDecoration.collapsed(
+                                  hintText: 'Precios actualizados al:',
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 0),
+                              padding:
+                                  const EdgeInsets.only(top: 10.0, bottom: 4.0),
+                              height: 45,
+                              width: 180,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: TextFormField(
+                                decoration: InputDecoration.collapsed(
+                                  hintText: snapshot.data[index].name,
+                                ),
+                                //controller: provinciaControll,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 19),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(left: 0),
+                              padding: const EdgeInsets.only(
+                                  top: 10.0, bottom: 20.0, left: 15),
+                              height: 95,
+                              width: 195,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: ButtonTheme(
+                                minWidth: 100.0,
+                                height: 50.0,
+                                child: RaisedButton(
+                                  onPressed: () {},
+                                  textColor: Colors.white,
+                                  color: Colors.amber,
+                                  child: Text(
+                                    "Guardar Cambios",
+                                    style: TextStyle(
+                                      fontSize: 17.0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 0),
+                              padding: const EdgeInsets.only(
+                                  top: 10.0, bottom: 20.0, right: 20),
+                              height: 95,
+                              width: 180,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: ButtonTheme(
+                                minWidth: 100.0,
+                                height: 50.0,
+                                child: RaisedButton(
+                                  onPressed: () {},
+                                  textColor: Colors.white,
+                                  color: Colors.amber,
+                                  child: Text(
+                                    "Ver Histórico",
+                                    style: TextStyle(
+                                      fontSize: 17.0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
                           ],
                         ),
                       ],
-
+                    ),
                   );
                 },
               );
